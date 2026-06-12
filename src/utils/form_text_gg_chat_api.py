5e160c2486bb6cb37c35033dbd016a80be5213e0
vuong-qc <vuong.qc@pls.edu.vn>
2026-06-12 16:25:04 +0700

    Sync source for production release

--- src/utils/form_text_gg_chat_api.py
+++ src/utils/form_text_gg_chat_api.py
@@ -3,7 +3,7 @@ from src.enums.text_format_enum import TextFormatEnum
 class FormatContentGgChatAPI:
     @staticmethod
     def format_content_checkin(user_name:str, tasks: list[str]):
-        content = TextFormatEnum.CHECKIN.format(time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), user_name=user_name)
+        content = TextFormatEnum.CHECKIN.format(time=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), user=user_name)
 
         task_lines = '\n'.join(
             f'{TextFormatEnum.TASK_PREFIX}{task}'
